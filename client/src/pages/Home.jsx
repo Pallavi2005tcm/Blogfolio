9f66bf23a84ac4633a3d0dbda9d7fc3aa48c3226
Pallavi_Singh_1516 <154963629+Pallavi2005tcm@users.noreply.github.com>
2026-01-29 22:02:43 +0530

    Fix import statement for Navbar component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from "../components/Navbar";
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
 import Newsletter from '../components/Newsletter'
